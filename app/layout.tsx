c222c585aea12942c2f493e3234ff3cdea56942a
Nicolas Lobo <nicolaslobo9@gmail.com>
2025-11-28 21:27:11 -0300

    fix: Hydration error caused by browser extensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${robotoMono.variable} antialiased bg-black text-white font-sans`}
       >
